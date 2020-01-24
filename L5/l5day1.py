0287fe5d468c9dfbf6c9e223e4fe979e6dfd42ad
udanzo-p <59428879+udanzo-p@users.noreply.github.com>
2020-01-24 17:38:24 +0530

    Add files via upload

---
+++ L5/l5day1.py
@@ -0,0 +1,11 @@
+dict={}
+dict['one']="This is  one"
+dict[2]="Try to understand"
+
+tinydict={'name': 'brawn','decode':6734,'dept':'sales'}
+
+print(dict['one'])
+print(dict[2])
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
