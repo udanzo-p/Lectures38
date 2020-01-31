e8f07f007bc86e8f2e6ead2bab8bc3698b56dc00
U-danz0 <59428879+udanzo-p@users.noreply.github.com>
2020-02-01 01:32:51 +0530

    Add files via upload

---
+++ Lecture 8 programming exercise/areaofcircle.py
@@ -0,0 +1,7 @@
+import math
+print("Enter Radius to compute area & circumference of circle")
+a=input()
+pi=math.pi
+Area=pi*(int(a)*int(a))
+circumference=2*pi*(float(a))
+print("The area & circumference of circle are resp.",Area,circumference)
---
+++ Lecture 8 programming exercise/factorial.py
@@ -0,0 +1,8 @@
+print("Enter the no. whose factorial is to be found")
+var=1
+n=input()
+a=int(n)
+while a>0:
+    var=var*a
+    a=a-1
+print("Factorial is ",var)
