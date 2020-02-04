65128508931573517f4ef850d699ea95c780258c
U-danz0 <59428879+udanzo-p@users.noreply.github.com>
2020-02-04 14:24:41 +0530

    Add files via upload

---
+++ Lecture 8 programming exercise/sumofnolistwithinput.py
@@ -0,0 +1,10 @@
+print(" List to claculate sum")
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+list=[a,b,c,d,e]
+sum=sum(list)
+print("The sum of elements in list is",sum)
